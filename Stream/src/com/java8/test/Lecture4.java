b133a607c02c7cc0afbd981f66a0d915e5d9b211
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-09-17 04:20:01 -0400

    Ading employee Json

--- Stream/src/com/java8/test/Lecture4.java
+++ Stream/src/com/java8/test/Lecture4.java
@@ -2,7 +2,6 @@ package com.java8.test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.junit.Ignore;
---
+++ Stream/src/com/stream/model/Employee.java
@@ -0,0 +1,65 @@
+package com.stream.model;
+
+public class Employee {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private int deptId;
+	private int age;
+	private double salary;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", deptId=" + deptId
+				+ ", age=" + age + ", salary=" + salary + "]";
+	}
+
+}
